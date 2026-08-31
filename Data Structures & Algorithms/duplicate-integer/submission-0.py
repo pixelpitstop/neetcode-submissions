1252ae17cade8d877155ddf904bf15c4e7005397
Jayasuryaa_Chandrasekar <jayasuryaa1785@outlook.com>
2026-08-31 10:44:26 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen= set()
+
+        for num in List: 
+            if num in seen:
+                seen.add(num)
+            return True
+        return False 
